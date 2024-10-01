7363bc13507c3d8104279caa31d7026390317bf8
shubaivan <shuba.ivan@yandex.ru>
2024-10-01 17:45:05 +0300

    fix auth_date

--- migrations/Version20240920144744.php
+++ migrations/Version20240920144744.php
@@ -20,7 +20,7 @@ final class Version20240920144744 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE telegram_user ADD auth_date INT NOT NULL');
+        $this->addSql('ALTER TABLE telegram_user ADD auth_date INT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
